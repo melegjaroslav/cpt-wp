d2b45bce482a923955c5d8fda3134b2905184f0a
Jaroslav Meleg <meleg.jaroslav@gmail.com>
2016-11-09 10:36:53 +0100

    comm

--- posttypes.php
+++ posttypes.php
@@ -9,8 +9,6 @@
 
 add_action( 'init', 'my_custom_post_type' );
 
-//This function must always be last!
-//Do NOT leave spaces after this function!
 function my_custom_post_type() {
-	require( 'post-args.php' );
+		include_once( 'post-args.php' );
 }
